8a9df041a53451e924360dcf80bebd71a950383b
Peter Murray <jester@dltj.org>
2023-01-04 21:07:09 -0500

    Rewrite Zoom-to-S3 code
    This code was arbitrarily failing for closed caption files.  It seemed
    that the Lambda was only receiving part of each file from Zoom, and the
    multi-part upload was failing (EntityTooSmall). In contrast, the method
    suggested at [Python program to stream data from a URL and write it to
    S3 ? – All About
    Tech](https://amalgjose.com/2020/08/13/python-program-to-stream-data-from-a-url-and-write-it-to-s3/)
    uses `s3.upload_fileobj()`.

--- serverless_zoom_recordings/retrieve_recording.py
+++ serverless_zoom_recordings/retrieve_recording.py
@@ -1,15 +1,21 @@
 """
 Copy recording from Zoom to S3 bucket.
 
-This work is based heavily on [multi-part-upload/handler.py](https://github.com/keithrozario/multi-part-upload/blob/master/serverless/handler.py) from Keith Rozario.
+This work is based heavily on [Python program to stream data from a URL and write it to S3](https://amalgjose.com/2020/08/13/python-program-to-stream-data-from-a-url-and-write-it-to-s3/) from Amal G Jose.
 """
+import http.client as http_client
 import json
+import logging
 import os
+import urllib
 
 import boto3
 import requests
 import structlog
 from botocore.exceptions import ClientError
+from requests.adapters import HTTPAdapter
+from requests.models import PreparedRequest
+from urllib3 import Retry
 
 from .util.identifiers import parse_organization
 from .util.log_config import setup_logging
@@ -20,6 +26,21 @@ CHUNK_SIZE = 5 * 1024 * 1024
 
 s3 = boto3.resource("s3")
 s3_client = boto3.client("s3")
+
+
+def prepped_request_dict(prepped, encoding=None):
+    # Based on https://stackoverflow.com/a/60058128/201674
+    # prepped has .method, .path_url, .headers and .body attribute to view the request
+    encoding = encoding or requests.utils.get_encoding_from_headers(prepped.headers)
+    body = prepped.body.decode(encoding) if encoding else "<binary data>"
+    request_dict = {
+        "method": prepped.method,
+        "path_url": prepped.path_url,
+        "body": body,
+    }
+    request_dict["headers"] = prepped.headers
+    ## "\n".join(["{}: {}".format(*hv) for hv in prepped.headers.items()])
+    return request_dict
 
 
 def handler(sf_input, context):
@@ -55,77 +76,80 @@ def handler(sf_input, context):
         raise RuntimeError("_recording_id not found in step function input")
     sf_output = {"_recording_id": recording_id}
 
-    ##STAGE Set up retrieval
-    stage = "Set up retrieval"
-    file_extension = f".{sf_input['extension']}" if "extension" in sf_input else ""
-    s3_key = f"{sf_input['_recording_id']}/{sf_input['recording_type']}{file_extension}"
-    log.debug(
-        stage,
-        reason="Creating multipart upload",
-        s3_key=s3_key,
-        bucket=RECORDINGS_BUCKET,
-        chunk_size=CHUNK_SIZE,
-        url=sf_input["download_url"],
-    )
-    response = s3_client.create_multipart_upload(
-        Bucket=RECORDINGS_BUCKET,
-        Key=s3_key,
-        ContentType=sf_input["mime_type"],
-        Tagging=f"Purpose=recording-site-{parse_organization(sf_input['zoom_parent_meeting_topic'])}",
-    )
-    upload_id = response["UploadId"]
-    log = log.bind(upload_id=upload_id)
-    log.debug(stage, reason="Created multipart_upload", response=response)
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.propagate = True
 
     ##STAGE File transfer
     stage = "File transfer"
+    file_extension = f".{sf_input['extension']}" if "extension" in sf_input else ""
+    s3_key = f"{sf_input['_recording_id']}/{sf_input['recording_type']}{file_extension}"
+    req = PreparedRequest()
+    req.prepare_url(
+        sf_input["download_url"], {"access_token": sf_input["download_token"]}
+    )
+    log.debug(
+        stage,
+        reason="Ready to start retrieval",
+        details={
+            "file_extension": file_extension,
+            "s3_key": s3_key,
+            "zoom_url": req.url,
+        },
+    )
 
-    parts = []
-    url = f"{sf_input['download_url']}?access_token={sf_input['download_token']}"
-    with requests.get(url, stream=True) as r:
-        r.raise_for_status()
+    # url = f"{sf_input['download_url']}?access_token={sf_input['download_token']}"
+    zoom_session = requests.Session()
+    adapter = HTTPAdapter(
+        max_retries=Retry(
+            total=4,
+            backoff_factor=1,
+            allowed_methods=None,
+            status_forcelist=[429, 500, 502, 503, 504],
+        )
+    )
+    zoom_session.mount("http://", adapter)
+    zoom_session.mount("https://", adapter)
+    s3_object = s3.Object(RECORDINGS_BUCKET, s3_key)
+
+    # We're turning on some extra logging here because upload_fileobj() is an async function
+    requests_log.setLevel(logging.DEBUG)
+    http_client.HTTPConnection.debuglevel = 1
+    with zoom_session.get(req.url, stream=True, timeout=10) as zoom_response:
+        zoom_response.raise_for_status()
         log.debug(
-            stage, reason="Response headers from Zoom", response_headers=r.headers
+            stage,
+            reason="Response headers from Zoom",
+            response_headers=zoom_response.headers,
         )
 
-        # download & upload chunks
-        for part_number, chunk in enumerate(r.iter_content(chunk_size=CHUNK_SIZE)):
-            response = s3_client.upload_part(
+        with zoom_response as part:
+            part.raw.decode_content = True
+            s3_transfer_conf = boto3.s3.transfer.TransferConfig(
+                multipart_threshold=10000, max_concurrency=4
+            )
+            s3_client.upload_fileobj(
+                part.raw,
                 Bucket=RECORDINGS_BUCKET,
                 Key=s3_key,
-                UploadId=upload_id,
-                PartNumber=part_number + 1,
-                Body=chunk,
-            )
-            log.debug(
-                stage, reason=f"Uploaded part {part_number + 1}", response=response
-            )
-            parts.append(
-                {
-                    "ETag": response["ETag"],
-                    "PartNumber": part_number + 1,
-                }
+                Config=s3_transfer_conf,
+                ExtraArgs={"ContentType": sf_input["mime_type"]},
             )
 
-    ##STAGE Complete multi-part upload
-    stage = "Complete multi-part upload"
-    log.debug(stage, reason="Completing", parts=parts)
-    try:
-        response = s3_client.complete_multipart_upload(
-            Bucket=RECORDINGS_BUCKET,
-            Key=s3_key,
-            UploadId=upload_id,
-            MultipartUpload={"Parts": parts},
-        )
-    except ClientError as ex:
-        log.error(stage, reason=ex.response["Error"]["Code"], response=ex.response)
-        raise RuntimeError from ex
+        try:
+            s3_object.wait_until_exists()
+        except ClientError as error:
+            log.error(stage, reason="AWS s3.upload_fileobj() error", details=error)
+            raise error
 
-    log.debug(stage, reason="Completed", response=response)
-    sf_output = {
-        "location": response["Location"],
-        "eTag": response["ETag"],
-    }
+        # Turning off extra logging
+        requests_log.setLevel(logging.WARN)
+        http_client.HTTPConnection.debuglevel = 0
+
+        sf_output["eTag"] = s3_object.e_tag.strip('"')
+        sf_output[
+            "location"
+        ] = f"""https://{RECORDINGS_BUCKET}.s3.amazonaws.com/{urllib.parse.quote(s3_key, safe="~()*!.'")}"""
+
     log.info(stage, reason="File uploaded", details=sf_output)
     sf_output.update(sf_input)
 
